57c946f218a98e1cbaea4cff9dfb326bb04593c0
aslam <aslam.maqsood@gmail.com>
2022-07-18 23:01:18 +0200

    generate seed data in seeds.rb
    - add gem Faker for seed data generation

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -1,7 +1,41 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# frozen_string_literal: true
+
+require 'faker'
+
+def username
+  [*('a'..'z'), *('0'..'9')].shuffle[0,8].join
+end
+
+(1..100).each do
+  User.create!(
+    username: username,
+    email: Faker::Internet.email,
+    password: 'password',
+    password_confirmation: 'password' 
+  )
+end
+
+(1..100).each do
+  name = Faker::Commerce.product_name
+  slug = Faker::Number.hexadecimal + '-' + name.downcase.dasherize
+
+  Item.create!(
+    title: name,
+    slug: slug,
+    description: Faker::Hipster.paragraph,
+    image: Faker::LoremFlickr.image,
+    favorites_count: rand(1..100),
+    user_id: rand(1..100)
+  )
+end
+
+user_ids = User.pluck(:id)
+item_ids = Item.pluck(:id)
+
+(1..100).each do
+  Comment.create!(
+    body: Faker::Hipster.sentence,
+    user_id: user_ids.sample,
+    item_id: item_ids.sample
+  )
+end
