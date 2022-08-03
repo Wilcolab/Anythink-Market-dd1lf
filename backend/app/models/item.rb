edf572275f86bd078fe292ad91351ebd4368e221
aslam <aslam.maqsood@gmail.com>
2022-08-03 14:48:46 +0200

    add filter by title scope

--- backend/app/models/item.rb
+++ backend/app/models/item.rb
@@ -8,6 +8,8 @@ class Item < ApplicationRecord
   scope :sellered_by, ->(username) { where(user: User.where(username: username)) }
   scope :favorited_by, ->(username) { joins(:favorites).where(favorites: { user: User.where(username: username) }) }
 
+  scope :filter_by, ->(title) { where(title: title) }
+
   acts_as_taggable
 
   validates :title, presence: true, allow_blank: false
